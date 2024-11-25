da0850a19f0c0f2d4374ac54ffc24d7c815068bd
ecourse <smkang@codenuri.co.kr>
2024-11-25 10:34:03 +0900

    Add files via upload

---
+++ DAY1/01_using1.cs
@@ -0,0 +1,13 @@
+// 10 page
+using System;
+using static System.Console;  // C#6.0 부터 지원
+                              // 현재 C#은 13.0이 최신
+// 아래 코드의 설명이 8 page
+System.Console.WriteLine("C#");
+
+// using System 이 있으면 아래 처럼 사용가능
+Console.Write("Hello, ");
+Console.WriteLine("C#");
+
+// using static System.Console 이 있으면 아래 처럼도 가능
+WriteLine("C#");
