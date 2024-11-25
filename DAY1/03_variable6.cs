333be5458bb9f64fe722da94bc6283c22300abf6
ecourse <smkang@codenuri.co.kr>
2024-11-25 11:22:32 +0900

    Add files via upload

--- DAY1/03_variable6.cs
+++ DAY1/03_variable6.cs
@@ -6,9 +6,10 @@ int n2 = 20;
 // 변수 값 출력 하는 방법
 
 // #1. 변수 한개
-Console.WriteLine(n1);
+WriteLine(n1);
 
 // #2. 서식화된 문자열
-
+WriteLine("n1 = {0}, n2 = {1}", n1, n2);
 
 // #3. $ 문자열
+WriteLine($"n1 = {n1}, n2*2 = {n2*2}");
