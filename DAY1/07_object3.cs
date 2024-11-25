a84b9f6d3623c443c50bfd7e6a87ed59bbc456a4
ecourse <smkang@codenuri.co.kr>
2024-11-25 12:54:54 +0900

    Add files via upload

---
+++ DAY1/07_object3.cs
@@ -0,0 +1,13 @@
+// 모든것은 object 로 가르킬수 있다.
+int    n = 0;
+double d = 0;
+string s = "A";
+
+object o1 = n;
+object o2 = d;
+object o3 = s;
+
+// object 의 실제이름은 System.Object
+System.Object o4 = n;
+System.Object o5 = d;
+System.Object o6 = s;
