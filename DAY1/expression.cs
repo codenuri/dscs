9d710b641b6144030f3a5d655951dc8a98906034
ecourse <smkang@codenuri.co.kr>
2024-11-25 16:48:08 +0900

    Add files via upload

--- DAY1/expression.cs
+++ DAY1/expression.cs
@@ -1,11 +1,17 @@
 ﻿// expression.cs
 
 // statement(문장) : 세미콜론(;) 으로 끝나는 한줄의 코드. 실행단위
-// expression      : 하나의 표현식을 만드는 코드 집합
+// expression(표현식)  : 하나의 값을 만드는 코드 집합
 //                   하나의 값을 만들기 때문에 변수 초기값으로 사용가능
 
 int n = 2;
 
 n * 2 + 3;
 
-n = n * 2 + 3;
+int n2 = n * 2 + 3;
+
+// 요즘 언어들에서 유행하는 개념
+// RUST : if, switch 도 표현식이다.
+// C#   : switch 도 표현식이다.(if 는 안됨)
+
+// C/C++ 은 if, switch 는 statement 이고 expression 이 아님
