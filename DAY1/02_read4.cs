9b84e7f8fdbcb78c50b2743711e1c03caf8ff693
ecourse <smkang@codenuri.co.kr>
2024-11-25 10:57:57 +0900

    Add files via upload

---
+++ DAY1/02_read4.cs
@@ -0,0 +1,15 @@
+using System;
+
+// Console.Read() : 입력 버퍼에서 한자 꺼내기
+// int n = Console.Read();	// 입력 대기
+                            // enter 를 입력해야 
+                            // 입력완료
+
+Console.Write("press any key >> ");
+
+// 버퍼가 아닌 키보드에서 직접 입력
+// => ENTER 필요 없음.
+//ConsoleKeyInfo cki = Console.ReadKey();
+ConsoleKeyInfo cki = Console.ReadKey(true);
+
+Console.WriteLine(cki.KeyChar); 
