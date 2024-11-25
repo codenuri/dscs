545be046335d00c8ff7147c116c1680d1bf7643d
ecourse <smkang@codenuri.co.kr>
2024-11-25 12:57:09 +0900

    Add files via upload

---
+++ DAY1/07_object4.cs
@@ -0,0 +1,13 @@
+using static System.Console;
+
+string s = "abcd";
+
+// 메소드 (멤버 함수) : 객체(변수)이름.메소드이름()
+// 속성(Property) : 객체이름.속성이름   으로 사용 () 없음.
+//                  멤버 데이타 아닙니다.
+//                  3일차에 property  문법에서 자세히 설명
+bool b = s.Contains('b');
+int  n = s.Length;       
+
+WriteLine($"{b} {n}");
+
