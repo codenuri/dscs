d42a5ce505556b8c22ef514cb6b65222974b7df0
ecourse <smkang@codenuri.co.kr>
2024-11-25 10:10:01 +0900

    Add files via upload

---
+++ DAY1/00_entry1.cs
@@ -0,0 +1,14 @@
+﻿// entry - 5 page 
+// C# 1.0 부터 사용되던 방식
+// => 임의 클래스에 static 으로 Main 메소드 작성.
+
+// C++     : 멤버 함수 라는 용어 사용
+// C#/Java : 메소드 용어사용( 함수와 같은 의미)
+
+class Program
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("hello, C#");
+    }
+}
---
+++ DAY1/00_entry2.cs
@@ -0,0 +1,16 @@
+﻿// Top Level Programming
+// => C# 9.0 부터 지원
+// => Main 메소드 없이.. 소스의 첫번째 문장 부터 실행
+// => Python 과 유사. 
+// => 원리는 교재 6page
+
+System.Console.WriteLine("hello, C#");
+
+// 위소스 빌드해서 DAY1.dll 생성되게 하고
+// ILDASM 에서 
+// DAY1.dll 열어 보세요
+
+// 주의
+// C# 은 함수 오버로딩을 지원 하지만, top level 방식은 에러
+int square(int a) { return a * a; }
+double square(double a) { return a * a; }
