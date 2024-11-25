f9ef4e996835fa8a9f1d48a26a7fe0e6d1724d07
ecourse <smkang@codenuri.co.kr>
2024-11-25 12:42:14 +0900

    Add files via upload

---
+++ DAY1/04_cts.cs
@@ -0,0 +1,15 @@
+// CTS - 29 page
+using System;
+
+int   n1 = 0;
+Int32 n2 = 0;
+System.Int32 n3 = 0;
+//System.int   n4 = 0; // error
+
+double d1 = 0;
+Double d2 = 0;
+System.Double d3 = 0;
+
+string s1 = "A";
+String s2 = "A";
+System.String s3 = "A";
