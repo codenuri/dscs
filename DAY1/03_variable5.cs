87084dc705dc6353a170285f62f64dce359c6106
ecourse <smkang@codenuri.co.kr>
2024-11-25 11:19:54 +0900

    Add files via upload

---
+++ DAY1/03_variable5.cs
@@ -0,0 +1,20 @@
+using static System.Console;
+
+// casting - 21 page
+double d = 3.4;
+//int n1 = d;		// error. C/C++ 언어는 가능.
+
+int n2 = (int)d;	// ok. C언어와 동일한 방법. 
+
+
+// nameof - 22 page. C# 6.0
+// => 변수, 클래스, 속성의 이름을 "문자열" 변경
+int color = 100;  // nameof(color) => "color"
+string s = "abcd";
+
+WriteLine($"{nameof(color)} : {color}");
+				// "color"  : 100
+
+WriteLine($"{nameof(s.Length)} : {s.Length}");
+				// "Length" : 4
+
---
+++ DAY1/03_variable6.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+int n1 = 10;
+int n2 = 20;
+
+// 변수 값 출력 하는 방법
+
+// #1. 변수 한개
+Console.WriteLine(n1);
+
+// #2. 서식화된 문자열
+
+
+// #3. $ 문자열
