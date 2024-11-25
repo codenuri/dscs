abe36c4825bc18e0a2e7e909dfbc05c9820b2a08
ecourse <smkang@codenuri.co.kr>
2024-11-25 12:52:48 +0900

    Add files via upload

--- DAY1/07_object2.cs
+++ DAY1/07_object2.cs
@@ -3,6 +3,7 @@ using static System.Console;
 // System.Object
 // => C#의 대부분의 타입은 System.Object 로 부터 상속 받게 됩니다.
 // => 7개의 메소드를 가진 클래스
+// => ref struct 라는 것으로 만든 타입은 예외(C#9.0 ~ )
 
 // 사용자 코드		// 컴파일러가 변경한 코드
 class Car			// class Car  : System.Object
