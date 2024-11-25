9bfc7a744b40b032357afa4d774f6ed35e1eacb2
ecourse <smkang@codenuri.co.kr>
2024-11-25 10:45:22 +0900

    Add files via upload

---
+++ DAY1/02_read1.cs
@@ -0,0 +1,15 @@
+// 표준 입력
+// input. 15 page
+using System;
+
+Console.Write("input your name >> ");
+
+// #1. 문자열 입력
+string s = Console.ReadLine();	 
+
+Console.WriteLine(s);	   		
+ 
+      
+  
+  
+   
