c76513c08c2eff63f3e9ff281b4ad620b034657a
ecourse <smkang@codenuri.co.kr>
2024-11-26 11:13:41 +0900

    Add files via upload

---
+++ DAY2/06_static1.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+// 90 page
+
+
+
+class Car 
+{
+    private int speed = 0;
+
+    public int cnt = 0;
+
+    public Car() { ++cnt; }    
+}
+class Program
+{
+    public static void Main()
+    {
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+        // Car 객체를 몇대나 생성했는지 알고 싶다.
+        WriteLine($"{c1.cnt}");
+    }
+}
