69b7c74bc45a7daa81bf8b5608305cb3c178e51b
ecourse <smkang@codenuri.co.kr>
2024-11-26 14:36:09 +0900

    Add files via upload

--- DAY2/09_property2.cs
+++ DAY2/09_property2.cs
@@ -7,16 +7,17 @@ class Person
     public int Age
     {
         get { return age; } 
-        set { age = value; }
+        set { if( value > 0 && value < 150) age = value; }
     }
 }
-
 class Program
 {
     public static void Main()
     {
         Person p = new Person();
 
+        // property 는 사용시 public 필드처럼 사용합니다.
+        // => 메소드 처럼 만들고, 필드 처럼 사용하게 하는 문법
         p.Age = 10;
         int n = p.Age;
 
