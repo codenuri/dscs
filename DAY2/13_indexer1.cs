326d85b5c22fb70b1dc9e16ef7011c49c024c679
ecourse <smkang@codenuri.co.kr>
2024-11-26 15:57:14 +0900

    Add files via upload

--- DAY2/13_indexer1.cs
+++ DAY2/13_indexer1.cs
@@ -9,6 +9,7 @@ class Sentence
 
     public Sentence( string s)
     {
+        // 문자열을 단어별로 분리
         words = s.Split();
                 // s의 문자열을 단어별로 분리해서 배열로 만들어서 반환
                 // new string[] { "Dog", "is", "Animal"}
@@ -16,9 +17,19 @@ class Sentence
 
     public string Text
     {
+        // 단어들을 다시 문자열로 조립
         get { return string.Join(" " , words); }
     }
 
+    // indexer 만드는 법
+    // 1. property 와 거의 유사
+    // 2. 이름 위치에 this[int idx] 넣으세요
+    //    "Text" => "this[int idx]" 로 변경
+    public string this[int idx]
+    {
+        get { return words[idx]; }
+        set { words[idx] = value; }
+    }
 }
 
 class Program
@@ -27,10 +38,9 @@ class Program
     {
         Sentence s = new Sentence("Dog is Animal");
 
-//		WriteLine( s[2] ); 	// Animal
+		WriteLine( s[2] ); 	// Animal
+        s[0] = "Cat";
 
-//      s[0] = "Cat";
-
-      WriteLine(s.Text);	// Cat is Animal
+        WriteLine(s.Text);	// Cat is Animal
     }
 }
