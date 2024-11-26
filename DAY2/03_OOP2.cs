30ce3de17ce150f5e6faa654255282b7feacdcf5
ecourse <smkang@codenuri.co.kr>
2024-11-26 10:35:58 +0900

    Add files via upload

---
+++ DAY2/03_OOP2.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+// 객체지향 프로그래밍 개념
+// => 필요한 타입을 먼저 만들자!!
+// => 타입을 만드는 문법을 제공하는 언어가 "객체지향 언어"
+
+class Rect
+{
+    public int x1 = 0;
+    public int y1 = 0;
+    public int x2 = 0;
+    public int x2 = 0;
+
+    public int GetArea() { return (x2 - x1) * (y2 - y1); }
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
+
+
