2ad77962334fd6f9c3d40eb73a5c9491ec5bc8c7
ecourse <smkang@codenuri.co.kr>
2024-11-26 12:38:43 +0900

    Add files via upload

---
+++ DAY2/06_static2.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+
+class Car
+{
+    private int speed = 0;
+
+    private static int cnt = 0; 
+
+    public Car() { ++cnt; }
+
+    public int GetCount() {  return cnt; }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+//      Car.cnt = 100;
+
+        WriteLine($"{c1.GetCount()}");
+    }
+}
