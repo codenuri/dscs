92bb4b35b65441616124e4fbcb5b475f4d4ed9ab
ecourse <smkang@codenuri.co.kr>
2024-11-26 17:01:08 +0900

    Add files via upload

---
+++ DAY2/partial.cs
@@ -0,0 +1,17 @@
+﻿// 클래스를 여러개 파일로 나누어 작업가능합니다.
+// a.cs
+partial class Example
+{
+    public void M1() { }
+}
+
+// b.cs
+partial class Example
+{
+    public void M2() { }
+}
+
+// c.cs
+Example e = new Example();
+e.M1();
+e.M2();
