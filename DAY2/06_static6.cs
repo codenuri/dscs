46d9d8aa0c9afad0c56521a9c2fa5a3d58541f98
ecourse <smkang@codenuri.co.kr>
2024-11-26 13:40:17 +0900

    Add files via upload

---
+++ DAY2/06_static6.cs
@@ -0,0 +1,13 @@
+﻿// C# entry point 규칙 
+// => 임의의 클래스를 만들고
+// => Main 이라는 이름을 가진 static method 를 만들어야 한다.
+
+class Program
+{
+    public void M1() { }
+
+    public static void Main()
+    {
+        M1(); //??
+    }
+}
