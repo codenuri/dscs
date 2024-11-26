c55bbeaf3b0980866685372605a4f27948b0b28b
ecourse <smkang@codenuri.co.kr>
2024-11-26 09:18:09 +0900

    Add files via upload

--- DAY2/02_parameter_modifier1.cs
+++ DAY2/02_parameter_modifier1.cs
@@ -14,9 +14,10 @@ class Program
 
 	// ref modifier
 	// => 복사본이 아닌 참조로 받아달라는 의미
+	// => 받을때, 호출할때 모두 표기해야 합니다. 
     public static void Inc2(ref int x)
     {
-        ++x;    
+        ++x;    // 복사본 아닌 원본 증가( 79 page 아래 그림)
     }
     public static void Main()
 	{
@@ -26,7 +27,15 @@ class Program
 
         Inc2(ref n);	// 참조로 전달해 달라.
         
-        WriteLine(n);	// 1
+        WriteLine(n);   // 1
 
+		// C# 1.0 ~ 에서는 ref 는 메소드 인자만 가능했습니다.
+		// C# 7.0 부터 지역변수로도 ref 사용가능합니다.
+
+		ref int r = ref n;  // ref local 이라는 문법
+
+		r = 20;
+
+		WriteLine(n); // 20 
     }
 }
