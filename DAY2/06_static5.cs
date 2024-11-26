ec2e10b169db5eab1b02dc5798564f1b44dde9a4
ecourse <smkang@codenuri.co.kr>
2024-11-26 13:34:37 +0900

    Add files via upload

--- DAY2/06_static5.cs
+++ DAY2/06_static5.cs
@@ -4,6 +4,8 @@
     private static int data2 = 0;
 
     // 아래 코드에서 에러를 모두 고르세요
+    // static field : 객체가 없어도 메모리에 존재, 모든 객체가 공유
+    // static method : 객체없이 호출 가능
     public void M1()
     {
         data1 = 0;
