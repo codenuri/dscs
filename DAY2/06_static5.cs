6f1c09aac936c78904f7b55cf25676ab1c2b0933
ecourse <smkang@codenuri.co.kr>
2024-11-26 13:32:54 +0900

    Add files via upload

---
+++ DAY2/06_static5.cs
@@ -0,0 +1,24 @@
+﻿class Example
+{
+    private int data1 = 0;
+    private static int data2 = 0;
+
+    // 아래 코드에서 에러를 모두 고르세요
+    public void M1()
+    {
+        data1 = 0;
+        data2 = 0;
+    }
+    public static void M2()
+    {
+        data1 = 0;
+        data2 = 0;
+    }
+}
+class Program
+{
+    public static Main()
+    {
+
+    }
+}
