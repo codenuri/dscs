72662ce3f910e96cc0da9ef05412a9447ce2f54e
ecourse <smkang@codenuri.co.kr>
2024-11-26 14:33:29 +0900

    Add files via upload

--- DAY2/09_property2.cs
+++ DAY2/09_property2.cs
@@ -2,17 +2,25 @@ class Person
 {
     private int age;
 
+    // 필드처럼 보이지만 {} 가 있고
+    // 메소드 처럼 보이기도 하지만 () 가 없습니다.
     public int Age
     {
-
+        get { return age; } 
+        set { age = value; }
     }
-
 }
 
 class Program
 {
     public static void Main()
     {
+        Person p = new Person();
+
+        p.Age = 10;
+        int n = p.Age;
+
+        Console.WriteLine(n);
  
     }
 }
