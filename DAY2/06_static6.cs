c4411bd3f2ec23916b9d3399b62dce565389f252
ecourse <smkang@codenuri.co.kr>
2024-11-26 13:44:51 +0900

    Add files via upload

--- DAY2/06_static6.cs
+++ DAY2/06_static6.cs
@@ -2,12 +2,20 @@
 // => 임의의 클래스를 만들고
 // => Main 이라는 이름을 가진 static method 를 만들어야 한다.
 
+// CLR 에서 "클래스이름.Main()" 으로 객체없이 호출. 
+
 class Program
 {
     public void M1() { }
+    public static void M2() { }
 
     public static void Main()
     {
-        M1(); //??
+        M1(); // error. 객체가 없다.
+
+        M2(); // ok. Program.M2()
+
+        Program pg = new Program();
+        pg.M1(); // ok.. 객체를 가지고 호출. 
     }
 }
