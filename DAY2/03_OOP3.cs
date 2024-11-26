a269a8cc23bd55e6fbbc095ef6e38aab25cb4ea2
ecourse <smkang@codenuri.co.kr>
2024-11-26 10:41:20 +0900

    Add files via upload

---
+++ DAY2/03_OOP3.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+class Rect
+{
+    public int x1 = 0;
+    public int y1 = 0;
+    public int x2 = 0;
+    public int x2 = 0;
+
+    public int GetArea() { return (x2 - x1) * (y2 - y1); }
+
+    // 생성자 : 객체를 만들때 자동으로 호출되는 메소드
+    //         이름이 클래스 이름과 동일
+    public Rect(int a, int b, int c, int d)
+    {
+        x1 = a; 
+        y1 = b;
+        x2 = c;
+        y2 = d;
+           
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+ 
+        Rect rect = new Rect(1, 1, 10, 10);
+        int ret = rect.GetArea();
+    }
+}
+
+
