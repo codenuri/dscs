62c457ced66f500073525b6d07b792a49da6e82e
ecourse <smkang@codenuri.co.kr>
2024-11-26 10:44:00 +0900

    Add files via upload

--- DAY2/03_OOP3.cs
+++ DAY2/03_OOP3.cs
@@ -11,22 +11,23 @@ class Rect
 
     // 생성자 : 객체를 만들때 자동으로 호출되는 메소드
     //         이름이 클래스 이름과 동일
-    public Rect(int a, int b, int c, int d)
-    {
-        x1 = a; 
-        y1 = b;
-        x2 = c;
-        y2 = d;
-           
-    }
+    public Rect(int a, int b, int c, int d) => (x1, y1, x2, y2) = (a, b, c, d);
+    //    {
+    //        x1 = a; 
+    //        y1 = b;
+    //        x2 = c;
+    //        y2 = d;
+    //        (x1, y1, x2, y2) = (a, b, c, d);
+
+    //    }
 }
 class Program
 {
     public static void Main()
-    {
- 
+    { 
         Rect rect = new Rect(1, 1, 10, 10);
-        int ret = rect.GetArea();
+
+        int ret = rect.GetArea(); // 81
     }
 }
 
