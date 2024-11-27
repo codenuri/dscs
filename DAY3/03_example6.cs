7616cb8f85dbfae6dfa880a6d6bdf9161ccf6c1d
ecourse <smkang@codenuri.co.kr>
2024-11-27 10:54:31 +0900

    Add files via upload

--- DAY3/03_example6.cs
+++ DAY3/03_example6.cs
@@ -6,11 +6,17 @@ int[] x2 = x1;
 
 // C# 은 디자인 패턴을 아주 많이 사용해서 설계된 언어 입니다.
 // (prototype, decorator, adapter 등 수많은 패턴을 적용한 언어)
+
 int[] x3 = (int[])x1.Clone(); // prototype 패턴
+                              // 캐스팅이 필요한 이유는 인터페이스 배운후 설명
+                              // 지금은 Clone 에 집중
 
 x1[0] = 10;
 
 // 아래 결과 예측해 보세요
-Console.WriteLine($"{x1[0]}");
-Console.WriteLine($"{x2[0]}");
-Console.WriteLine($"{x3[0]}");
+Console.WriteLine($"{x1[0]}"); // 10
+Console.WriteLine($"{x2[0]}"); // 10
+Console.WriteLine($"{x3[0]}"); // 1
+
+Console.WriteLine($"{object.ReferenceEquals(x1, x2)}"); // True
+Console.WriteLine($"{object.ReferenceEquals(x1, x3)}"); // False
