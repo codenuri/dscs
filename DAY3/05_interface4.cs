2d7603757b09d6410a2c5f4c9b719fd71b05aced
ecourse <smkang@codenuri.co.kr>
2024-11-27 13:51:50 +0900

    Add files via upload

--- DAY3/05_interface4.cs
+++ DAY3/05_interface4.cs
@@ -1,5 +1,27 @@
 ﻿using static System.Console;
 
+// C++언어 : 복잡하기로 유명하고, 문법이 많습니다
+
+// java(1994년발표) : C++의 복잡함은 template, 연산자 재정의, 포인터등이 있기 때문이다.
+//        java 는 이런 문법 사용하지 않겠다..
+
+// C# 1.0(2002년경) : template 지원 안됨. 그래서 아래처럼 만들수 밖에 없었다.
+/*
+interface IComparable
+{
+    int CompareTo(object obj);  
+}
+*/
+
+// C# 2.0 : template(generic)문법이 도입됩니다.
+//          사용자가 타입을 지정하는 문법(내일 자세한 문법 배웁니다.)
+// 그래서 C# 2.0 부터 아래 인터페이스 도입됩니다.
+interface IComparable<T>
+{
+    int CompareTo(T obj);
+}
+
+
 
 class Label : IComparable
 {
