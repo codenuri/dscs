1fd952c6ee996f6c9d2c9bf9fb4e1ddda144aa32
ecourse <smkang@codenuri.co.kr>
2024-11-27 12:38:05 +0900

    Add files via upload

--- DAY3/참고.cs
+++ DAY3/참고.cs
@@ -3,9 +3,17 @@
 void f1(int  n) { }
 void f2(int? n) { }
 
-f1(10);
-f2(10);
+f1(10);     // ok
+f1(null);   // error
 
-f1(null);
-f2(null);
-f2(null);
+f2(10);     // ok
+f2(null);   // ok
+
+int  n1 = 10;
+int? n2 = 10;
+
+f1(n1);
+f1(n2);
+
+f2(n1);
+f2(n2);
