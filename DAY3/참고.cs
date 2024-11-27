44dd2dfac97b1807784d9c0b2b670bea3b1a3f02
ecourse <smkang@codenuri.co.kr>
2024-11-27 12:41:48 +0900

    Add files via upload

--- DAY3/참고.cs
+++ DAY3/참고.cs
@@ -24,6 +24,8 @@ f2(n2); // ok
 // f(int) : int 만 가능 int? 안됨
 // f(int?): int, int? 모두 가능
 
+// string 같은 참조 타입은 string? 를 C# 9.0 부터 지원 
+
 // C# 9.0 까지 대부분 메소드는 
 // int CompareTo(object) 형태 있지만 C# 9.0 부터
 // int CompareTo(object?) 형태로 변경.  많이 헷갈려하는 부분
