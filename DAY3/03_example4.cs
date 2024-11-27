316e95587182821fa0794e84f9c583b211501a68
ecourse <smkang@codenuri.co.kr>
2024-11-27 10:11:21 +0900

    Add files via upload

--- DAY3/03_example4.cs
+++ DAY3/03_example4.cs
@@ -6,21 +6,22 @@ using System.Collections.Generic;
 //         그래야, 기반 클래스 참조로 해당 특징을 사용할수 있다.
 //         문법적 규칙이 아닌 디자인 규칙
 
-
+// 핵심 #2. 기반 클래스 메소드 중에 파생 클래스가 override 하게 되는 것은
+//          반드시 virtual 로 해야 한다.
+//          파생 클래스에서는 "override" 로 재정의
 class Shape
 {
     private int color = 0;
 
-    public void Draw() { WriteLine("draw Shape"); }
+    public virtual void Draw() { WriteLine("draw Shape"); }
 }
-
 class Rect : Shape
 {
-    public void Draw() { WriteLine("draw Rect"); }
+    public override void Draw() { WriteLine("draw Rect"); }
 }
 class Circle : Shape
 {
-    public void Draw() { WriteLine("draw Circle"); }
+    public override void Draw() { WriteLine("draw Circle"); }
 }
 
 class Program
