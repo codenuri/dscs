04ec10d591f255c423564897eb2ba1d75997c761
ecourse <smkang@codenuri.co.kr>
2024-11-27 09:55:14 +0900

    Add files via upload

--- DAY3/03_example2.cs
+++ DAY3/03_example2.cs
@@ -1,5 +1,7 @@
 ﻿using static System.Console;
-
+using System.Collections.Generic; // List<> 를 사용하려면 필요
+                                  // 그런데, C#10.0 부터
+                                  // DAY3/obj/debug/net8.0/DAY3.GlobalUsing.g.cs 파일에 있음.
 class Shape
 {
     private int color = 0;
@@ -25,16 +27,19 @@ class Program
             int cmd = int.Parse( Console.ReadLine() );
 
             if      (cmd == 1) c.Add(new Rect());
-            else if (cmd == 2) c.Add( new Circle());
+            else if (cmd == 2) c.Add(new Circle());
             else if (cmd == 9)
             {
                 foreach( var s in c)
-                    s.Draw();
+                    s.Draw();   // A. Error.
             }
         }
     }
 }
 // 위 코드는 어디서 ? 왜?? 에러일까요 ?
+// => A 부분에서 s 의 타입은 Shape 입니다. List<Shape> 이므로
+// => Shape 에는 Draw가 없어서 에러!
 // 해결책은 뭘까요 ?
+// => 다음소스에서.
 
 
