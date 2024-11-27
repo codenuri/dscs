2db874f62900f29a34c6f13babb3679803f8ed1e
ecourse <smkang@codenuri.co.kr>
2024-11-27 10:38:02 +0900

    Add files via upload

--- DAY3/03_example5.cs
+++ DAY3/03_example5.cs
@@ -15,7 +15,18 @@ class Shape
 
 
     public virtual void Draw() { WriteLine("draw Shape"); }
+
+    // 자신의 복사본을 만드는 가상 메소드는 아주 널리 사용되는 기술입니다.
+    public virtual Shape Clone()
+    {
+        Shape shape = new Shape();
+        shape.color = color; // 이외의 모든 멤버를 복사합니다.
+        return shape;
+    }
 }
+
+
+
 class Rect : Shape
 {
     public override void Draw() { WriteLine("draw Rect"); }
@@ -54,6 +65,17 @@ class Program
                 // k번째 만든 도형의 복사본을 만들어서 collection c 끝에 추가
                 // => 어떻게 해야 할까요 ?
                 // => k번째 도형은 뭘까요 ?
+
+                // 해결책 #1. 아래 처럼 했다면
+                // => OCP 를 만족하지 못하는 좋지 않은 디자인
+                /*
+                if (c[k] is Rect )
+                {
+                    Rect r = new Rect();
+                    // c[k] 의 모든 속성을 r에 동일하게 복사
+                    c.Add(r);
+                 }
+                */
             }
         }
     }
