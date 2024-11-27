9645634de69c456cc143c542d70c7fb339a60b88
ecourse <smkang@codenuri.co.kr>
2024-11-27 09:40:36 +0900

    Add files via upload

---
+++ DAY3/03_example1.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+// 파워 포인트 같은 프로그램을 생각해 봅시다.
+// 1. 모든 도형을 타입으로 설계하면 편리 합니다.
+
+class Rect
+{
+    // x, y, w, h, 생성자등이 필요 합니다 - 생략
+    public void Draw() { WriteLine("draw Rect"); }
+}
+
+
+
+
+class Program
+{
+    public static void Main()
+    {
+    }
+}
