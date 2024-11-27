6c96af4eeab004bc770dc66db1dfdd2cc337dbd7
ecourse <smkang@codenuri.co.kr>
2024-11-28 08:55:12 +0900

    Add files via upload

--- DAY4/01_boxing_example3.cs
+++ DAY4/01_boxing_example3.cs
@@ -4,7 +4,6 @@ using System.Diagnostics.CodeAnalysis;
 using static System.Console;
 
 
-
 struct Temperature : IComparable, 
                      IComparable<Temperature>, 
                      IEquatable<Temperature>
@@ -54,3 +53,12 @@ class Program
 }
 
 
+
+// Equals() : object 클래스가 제공하는 가상 메소드
+//            대부분의 타입이 가진 메소드
+//            거의 모든 객체는 Equals() 메소드가 있다.
+//            Boxing 등의 문제를 해결하기 위해 IEquatable<T> 제공 
+
+// CompareTo : 크기 비교가 가능한 타입만 구현하면 된다.
+//             IComparable 인터페이스로 약속되어 있다.
+//             Boxing 등의 문제를 해결하기 위해 IComparable<T> 가 제공
