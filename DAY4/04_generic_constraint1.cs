b3d4b5f24383bbd0cdb3203114bf61f0bcc30d85
ecourse <smkang@codenuri.co.kr>
2024-11-28 09:59:48 +0900

    Add files via upload

--- DAY4/04_generic_constraint1.cs
+++ DAY4/04_generic_constraint1.cs
@@ -21,15 +21,33 @@ class Program
     */
 
     // Max 를 Generic 으로 해보세요
-    public static int Max(int a, int b)
+    public static T Max<T>(T a, T b)
     {
-        return a < b ? b : a;
+//      return a < b ? b : a; // < 는 숫자 타입만 가능
+
+        // CompareTo 는 int 와 string 모두 가능.
+        // 하지만 에러 - 이유는 아래 Foo 참고
+        return a.CompareTo(b) < 0 ? b : a;         
     }
 
     public static void Main()
     {
 		WriteLine($"{Max(10, 20)}");
 		WriteLine($"{Max("AAA", "CC")}");
+
+        Foo(10, 20); 
+    }
+    public static void Foo<T>(T a, T b)
+    {
+        a.CompareTo(b); // a안에 CompareTo 가 있다는 보장이 없다.
+                        // 그래서 error
+
+
+        a.ToString(); // ok. 대부분의 타입에는 있다. onject 가 제공
+
+        // 핵심
+        // Generic 안에서는 인자에 대해서 object 타입으로
+        // 가능한 연산만 할수 있다.
 
     }
 }
