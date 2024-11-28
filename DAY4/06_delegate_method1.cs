b8442c359cef98428bfdbb37e7208879c4337079
ecourse <smkang@codenuri.co.kr>
2024-11-28 11:02:58 +0900

    Add files via upload

--- DAY4/06_delegate_method1.cs
+++ DAY4/06_delegate_method1.cs
@@ -17,8 +17,13 @@ class Program
         Test.SMethod(1); // static 메소드는 클래스 이름으로 호출
         t.IMethod(1);    // instance 메소드는 객체이름으로 호출
 
-        MyFunc f1 = ?;
-        MyFunc f2 = ?;
+        // #1. delegate 의 Program 클래스의 메소드 가 아닌 
+        //     다른 클래스 메소드 등록하기. 
+        MyFunc f1 = Test.SMethod;  // 클래스이름.static_method 로 등록
+        MyFunc f2 = t.IMethod;     // 객체이름.instance_method 로 등록
+                                
+                                // 결국 delegate 객체 f1, f2안에는
+                                // 메소드주소, 객체 정보등이 모두보관됩니다
 
         f1(10); // Test.SMethod(10)
         f2(10); // t.IMethod(10)
