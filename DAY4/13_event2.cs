7caab04edd7d990f16848abd42e9b50a3558063e
ecourse <smkang@codenuri.co.kr>
2024-11-28 15:36:31 +0900

    Add files via upload

--- DAY4/13_event2.cs
+++ DAY4/13_event2.cs
@@ -1,17 +1,22 @@
 using static System.Console;
 
+// 이 소스 핵심
+// delegate 를 FindIndex() 등의 메소드에 정책 함수 로 보내는 모델이 아니라
+
+// GUI 의 이벤트 처리를 위해서 등록했다가 사용하는 모델 
+
 
 delegate void ClickHandler();
-
 
 class Button
 {
     // ClickHandler 가 타입(클래스)이름 인것을 헷갈리지 마세요
-    public ClickHandler Click = null;
+    public ClickHandler? Click = null;
 
     public void UserPressButton()
     {
-        Click();
+        // Click();
+        Click?.Invoke(); // null 이 아닐때만 사용. 
     }
 }
 
