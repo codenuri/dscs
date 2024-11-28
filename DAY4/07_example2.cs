e401011bb69139c591ca8866a02d0775f628c3e6
ecourse <smkang@codenuri.co.kr>
2024-11-28 13:39:40 +0900

    Add files via upload

--- DAY4/07_example2.cs
+++ DAY4/07_example2.cs
@@ -1,12 +1,27 @@
 using static System.Console;
 
+
+delegate bool MyFunc(int a);
+
+
 class Program
 {
     // Array.FindIndex 를 직접 만들어 봅시다.
-    public static int MyFindIndex()
-    {
 
+    public static int MyFindIndex(int[] arr, MyFunc match  )
+    {
+        // 여기서 match 는 사용자가 전달한 메소드(호출정보)를 담는 객체
+
+        for( int i = 0; i < arr.Length; i++)
+        {
+            if (match(arr[i]) == true)
+                return i;
+        }
+        return -1;
     }
+
+
+
     public static void Main()
     {
         int[] x = { 1, 8, 6, 4, 3 };
@@ -17,6 +32,9 @@ class Program
 
         WriteLine($"{ret2}"); // 2
     }
+
+
+
 
 
 
