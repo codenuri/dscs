44b5eef5956bc9318f346a4ba7e543a1d2e3fe24
ecourse <smkang@codenuri.co.kr>
2024-11-28 15:34:00 +0900

    Add files via upload

--- DAY4/13_event2.cs
+++ DAY4/13_event2.cs
@@ -1,10 +1,17 @@
 using static System.Console;
 
+
+delegate void ClickHandler();
+
+
 class Button
 {
+    // ClickHandler 가 타입(클래스)이름 인것을 헷갈리지 마세요
+    public ClickHandler Click = null;
+
     public void UserPressButton()
     {
-        WriteLine("To Do Something");
+        Click();
     }
 }
 
@@ -21,7 +28,7 @@ class Program
         btn1.Click = Foo; // event handler 등록
         btn2.Click = Goo; // event handler 등록
 
-        btn1.UserPressButton(); 
-        btn2.UserPressButton(); 		
+        btn1.UserPressButton(); // Foo 호출
+        btn2.UserPressButton(); // Goo 호출
     }
 }
