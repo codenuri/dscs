34ae2d50951928b8e66a0b1fae0ea6118e375650
ecourse <smkang@codenuri.co.kr>
2024-11-28 09:08:28 +0900

    Add files via upload

---
+++ DAY4/02_interface3.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        // C# 배열의 상속계층은
+        // int[] => System.Array => System.Object 입니다.
+        int[] x = { 1, 2, 3, 4, 5 };
+
+        Array arr = x; // ok. Array 가 기반 클래스 이므로
+
+    }
+}
