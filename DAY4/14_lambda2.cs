72573be0e326773bd106038f5c0f0f204553342e
ecourse <smkang@codenuri.co.kr>
2024-11-28 14:51:47 +0900

    Add files via upload

--- DAY4/14_lambda2.cs
+++ DAY4/14_lambda2.cs
@@ -3,7 +3,11 @@ using static System.Console;
 
 class Program
 {
-    static bool IsMultipleOf(int n) { return n % 2 == 0; }
+    static bool IsMultipleOf(int n) 
+    { 
+        // 여기서는 Main 메소드의 지역변수에 접근할수 없습니다.
+        return n % 2 == 0; 
+    }
 
     public static void Main()
     {
@@ -14,14 +18,26 @@ class Program
 
         // 배열에서 1번째 나오는 k의 배수를 찾고 싶다.
         // => IsMultipleOf 에서 k의 배수를 찾을수 있을까요 ?
-        int idx = Array.FindIndex(array, IsMultipleOf);
+        //    (k 는 Main 메소드의 지역변수 )
+//      int idx = Array.FindIndex(array, IsMultipleOf);
 
 
+        // 람다 표현식의 장점.
+        // => 자신이 포함된 메소드의 지역변수를 사용할수 있다.
 
-
-        int idx = Array.FindIndex(array, n => n % 2 == 0);
+        int idx = Array.FindIndex(array, n => n % k == 0);
 
 
         WriteLine($"{idx}");	// 3	
     }
 }
+
+// FindIndex(x, IsEven);          // FindIndex 에 "메소드(동작)"만 전달
+// FindIndex(x, n => n % k == 0); // FindIndex "동작 + Main의 지역변수 전달"
+
+// Closure : 자신이 포함된 문맥의 지역변수를 캡쳐할수 있는 일급객체
+// 일급객체 : 인자로 전달 가능한 객체
+//            함수(메소드)도 인자로 전달가능하므로 메소드도 일급객체
+
+// 일반 메소드는 클로져가 아니지만
+// 람다 표현식은 클로져가 될수 있습니다.
