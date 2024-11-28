dd364fc914d6a4adf861129801eb0eafe2b6ecad
ecourse <smkang@codenuri.co.kr>
2024-11-28 09:50:56 +0900

    Add files via upload

--- DAY4/03_generic2.cs
+++ DAY4/03_generic2.cs
@@ -2,8 +2,11 @@
 
 class Point<T>
 {
-    private T x = 0;
-    private T y = 0;
+    //  private T x = 0;
+    //  private T y = 0; // error. 임의 타입이 0으로 초기화 될수 없다.
+
+    private T x = default(T);
+    private T y = default;
 
     public Point(T a, T b)
     {
@@ -16,7 +19,9 @@ class Program
 {
     public static void Main()
     {
-        Point p1 = new Point(1, 2);
-        Point p2 = new Point(1.1, 2.2);
+        // generic class 는 타입인자 생략할수 없습니다.
+        // 반드시 전달해야 합니다.
+        Point<int> p1 = new Point<int>(1, 2);
+        Point<double> p2 = new Point<double>(1.1, 2.2);
     }
 }
