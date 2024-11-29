6f544c7f07213caf83ce0ea8020b3823590b83af
ecourse <smkang@codenuri.co.kr>
2024-11-29 14:12:04 +0900

    Add files via upload

--- DAY5/exception1.cs
+++ DAY5/exception1.cs
@@ -4,11 +4,16 @@
 
 class Database
 {
-    public void Backup() 
+    // 방법 #1. 실패로 약속된 값 반환. 
+    // 단점 : 호출자가 실패에 대한 처리를 하지 않을수도 있다.
+    //       "실패에 대한 처리를 강제 할수 없을까 ?"
+
+
+    public int Backup() 
     {
         // 실패 했다.. 호출자 에게 알려야 한다.
+        return -1;
     }
-
     public void Remove() { }
 }
 
