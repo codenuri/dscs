b3fb296785ec1dd71068dfdbff61e9edf5d67fa8
ecourse <smkang@codenuri.co.kr>
2024-11-29 15:15:48 +0900

    Add files via upload

--- DAY5/ex5.cs
+++ DAY5/ex5.cs
@@ -4,12 +4,27 @@ using System.Windows;
 // Step 5. Window 의 이벤트 처리는 Window 클래스가 처리하자.
 // Window 의 파생 클래스 만들기 
 
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.Title = "Hello, WPF";
+
+        this.MouseLeftButtonDown += LeftDown;
+    }
+
+    private void LeftDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        MessageBox.Show("Click");
+    }
+}
+
 class Program
 {
     [STAThread]
     public static void Main()
     {
-        Window win = new Window();
+        MainWindow win = new MainWindow();
         win.Show();
 
         Application app = new Application();
