9315df719ce6d4e6841c413a889c99f388faae56
ecourse <smkang@codenuri.co.kr>
2024-11-29 15:36:02 +0900

    Add files via upload

---
+++ DAY5/ex8.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+// Step 8. UI 는 C# 코드가 아닌 XML로 !!
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        StackPanel sp = null;
+
+        FileStream fs = new FileStream("../../../first.xaml", FileMode.Open);
+
+        sp = (StackPanel)XamlReader.Load(fs);
+
+        fs.Close(); // 파일 닫기 
+
+
+
+        this.Content = sp;
+    }
+
+}
+
+
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        MainWindow win = new MainWindow();
+        win.Show();
+
+        Application app = new Application();
+        app.Run();
+
+    }
+
+
+}
