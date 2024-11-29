ba457323bb79c2a31d551e7bd809482bf865140d
ecourse <smkang@codenuri.co.kr>
2024-11-29 15:04:12 +0900

    Add files via upload

---
+++ DAY5/ex4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+// Step 4. Property 와 event
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Window win = new Window();
+        win.Show();
+
+
+        Application app = new Application();
+        app.Run(); 
+
+    }
+}
