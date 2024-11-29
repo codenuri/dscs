ae06ff828bb19f2f5ec369d7ae74e5ee23d71101
ecourse <smkang@codenuri.co.kr>
2024-11-29 14:58:53 +0900

    Add files via upload

---
+++ DAY5/ex2.cs
@@ -0,0 +1,19 @@
+﻿// ex2.cs
+using System;
+using System.Windows;
+
+// Step 2. Window 만들기
+// => Main 위에 [STAThread] 추가
+// => Window 클래스 객체 생성후 사용. 
+
+class Program
+{
+    [STAThread]    
+    public static void Main()
+    {   
+        Window win = new Window();
+        win.Show();        
+
+        MessageBox.Show("Hello, WPF");
+    }
+}
