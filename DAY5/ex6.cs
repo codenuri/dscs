c2ffdde7ccca157bd5ed43fec120e0337e11c406
ecourse <smkang@codenuri.co.kr>
2024-11-29 15:19:16 +0900

    Add files via upload

---
+++ DAY5/ex6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+// Step 6. Contents 속성
+
+class MainWindow : Window
+{
+    public MainWindow()
+    {
+        // 핵심 : 윈도우는 Contents 속성으로 연결된 객체 한개를
+        //        화면에 보여주는 능력이 있습니다.
+//      this.Content = "HELLO"; // Title 이 아닌 윈도우 화면 0, 0에 
+                                // 문자열이 아닌 그림도 됩니다.
+                                // 왠만한것은 다 됩니다.
+
+
+        Button btn = new Button();
+        
+        this.Content = btn;
+
+    }
+
+}
+
+
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        MainWindow win = new MainWindow();
+        win.Show();
+
+        Application app = new Application();
+        app.Run();
+
+    }
+
+
+}
