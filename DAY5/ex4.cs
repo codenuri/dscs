7dd0977a15213c1f19c7bfc56ac19d24dd7e138c
ecourse <smkang@codenuri.co.kr>
2024-11-29 15:10:30 +0900

    Add files via upload

--- DAY5/ex4.cs
+++ DAY5/ex4.cs
@@ -11,9 +11,25 @@ class Program
         Window win = new Window();
         win.Show();
 
+        // #1. Property 설정
+        win.Title = "Hello, WPF";
+        win.Width = 500;
+        win.Height = 500;
+
+        // #2. 이벤트 처리
+        // btn.Click += Foo; // 어제 수업 delegate & event 생각해 보세요
+
+        win.MouseLeftButtonDown += Win_MouseLeftButtonDown;
+
+
 
         Application app = new Application();
         app.Run(); 
 
+    }
+
+    private static void Win_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        MessageBox.Show("Window Click");
     }
 }
