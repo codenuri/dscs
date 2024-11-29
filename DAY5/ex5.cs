99b3bcfd2d8bf5658301b816d81d7dfc09d214f1
ecourse <smkang@codenuri.co.kr>
2024-11-29 15:11:43 +0900

    Add files via upload

---
+++ DAY5/ex5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+// Step 5. Window 의 이벤트 처리는 Window 클래스가 처리하자.
+// Window 의 파생 클래스 만들기 
+
+class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Window win = new Window();
+        win.Show();
+
+        Application app = new Application();
+        app.Run();
+
+    }
+
+
+}
