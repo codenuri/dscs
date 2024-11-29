fb56f2bef52213e56f9be8a0195021123b25bc18
ecourse <smkang@codenuri.co.kr>
2024-11-29 11:14:27 +0900

    Add files via upload

--- DAY5/02_collection4.cs
+++ DAY5/02_collection4.cs
@@ -31,8 +31,25 @@ class Program
                   // 메모리 재할당이 있습니다.
                   // 비싼연산입니다.(느리다는 의미)
 
+                  // 메모리(capacity) 할당할때 기존 크기 * 2 가됩니다.
+                  // 그래서, 이후에 호출되는 Add 는 성능이 아주좋게 됩니다
+                  // C++, Java, C#, Python 모두 동일합니다.
 
-        WriteLine($"{c.Count}, {c.Capacity}");   // ? , ?
+        WriteLine($"{c.Count}, {c.Capacity}");   // 9 , 16
+
+        c.TrimExcess(); // 여분의 메모리 필요 없다.
+                        // 이순간 9개 메모리 재할당후
+                        // 기존 메모리 내용 => 9개 메모리 복사후
+                        // 기존 메모리 제거
+
+        WriteLine($"{c.Count}, {c.Capacity}"); // 9, 9
+
+        c.Clear(); // count 만 0으로 메모리는 계속 9
+
+        WriteLine($"{c.Count}, {c.Capacity}"); // 0, 9
+
+        c.TrimExcess();
+        WriteLine($"{c.Count}, {c.Capacity}"); // 0, 0
     }
 }
 
