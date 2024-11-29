46437d4808d235745440c0059b8c01d19881dfce
ecourse <smkang@codenuri.co.kr>
2024-11-29 10:05:52 +0900

    Add files via upload

---
+++ DAY5/01_array10.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+// index 이야기 (182 ~ )
+
+class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 1,2,3,4,5,6,7,8,9,10 };
+
+        arr[0] = 0; // 1번째 요소
+        arr[9] = 0; // 마지막(10번째 요소)
+
+        arr[^1] = 99; //  뒤에서 1번째
+
+        Console.WriteLine(arr[9]); // 99
+    }
+}
