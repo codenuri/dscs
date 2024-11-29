bafa228edbef92613e5a9bfc0f6a1a5e55b6dde6
ecourse <smkang@codenuri.co.kr>
2024-11-29 14:44:44 +0900

    Add files via upload

---
+++ DAY5/CSHARP_UI.cs
@@ -0,0 +1,11 @@
+﻿// CSHARP_UI.cs
+
+// 1990년대 GUI 
+// => C++ MFC, QT => 복잡합니다.
+
+
+// C# GUI 라이브러리 
+// #1. WinForm - 최초 GUI 라이브러리 
+// #2. WPF - 가장널리 사용된 버전, 이쁘고 쉽다.
+// #3. UWP - Windows 10 과 같이 등장(WPF 와 유사)
+// #4. WinUI => 현재 WinUI3 까지 등장
